4c4c7959594a715aeca1141a8272e5a717c42f23
RupaliShah <shah.rupali1@gmail.com>
2018-06-13 12:40:57 -0700

    jsonified all fields in univ_data

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import pandas as pd
+
 
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy import create_engine, func, desc
@@ -21,15 +21,27 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/univ_map/<latlon>")
+@app.route("/univ_map/latlondata")
 def univ_map():
-    coord = {}
-    univ_data = list(engine.execute('SELECT * FROM address_finances').fetchall())
-    majors = list(engine.execute('SELECT * FROM majors').fetchall())
-    for data[0] in univ_data:
-        coord["lon"] = data[8]
-        coord["lat"] = data[9]
-    return jsonify(coord)
+    location_data = {}
+    univ_data = engine.execute('SELECT * FROM address_finances').fetchall()
+    location_data['univ_id'] = [data[1] for data in univ_data]
+    location_data['name'] = [data[2] for data in univ_data]
+    location_data['address'] = [data[3] for data in univ_data]
+    location_data['city'] = [data[4] for data in univ_data]
+    location_data['zipcode'] = [data[5] for data in univ_data]
+    location_data['state_addr'] = [data[6] for data in univ_data]
+    location_data['web_addr'] = [data[7] for data in univ_data]
+    location_data['lon'] = [data[8] for data in univ_data]
+    location_data['lat'] = [data[9] for data in univ_data]
+    location_data['acceptance_rate'] = [data[10] for data in univ_data]
+    location_data['student_faculty_ratio'] = [data[11] for data in univ_data]
+    location_data['in_state_tuition'] = [data[12] for data in univ_data]
+    location_data['out_of_state_tuition'] = [data[13] for data in univ_data]
+    location_data['total_fin_aid'] = [data[14] for data in univ_data]
+    location_data['avg_amt_grant_aid'] = [data[15] for data in univ_data]
+    location_data['avg_amt_stu_loan'] = [data[16] for data in univ_data]    
+    return jsonify(location_data)
 
 if __name__ == "__main__":
     app.run(debug=True)
